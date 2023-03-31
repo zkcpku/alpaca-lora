d8a6439aad6fb500251daa84074e35ea3346cad7
zkcpku <1003180479@qq.com>
2023-03-31 14:12:24 +0800

    success with llama7b

--- main.sh
+++ main.sh
@@ -2,10 +2,10 @@ python finetune.py \
     --base_model 'decapoda-research/llama-7b-hf' \
     --data_path '/code/alpaca-lora-main/dataset/CSN_python_func_code_string.json' \
     --output_dir './lora-csn_python' \
-    --batch_size 128 \
-    --micro_batch_size 4 \
+    --batch_size 150 \
+    --micro_batch_size 50 \
     --num_epochs 3 \
-    --learning_rate 1e-4 \
+    --learning_rate 1e-3 \
     --cutoff_len 512 \
     --val_set_size 2000 \
     --lora_r 8 \
